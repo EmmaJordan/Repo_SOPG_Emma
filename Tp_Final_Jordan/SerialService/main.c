4d4e3c2162e619787c72de3f0d25da5bc250872f
Joan Emmanuel Jordán <ejjordan91@gmail.com>
2022-08-13 15:09:24 -0300

    hilos por separado para serial y TCP

--- Tp_Final_Jordan/SerialService/main.c
+++ Tp_Final_Jordan/SerialService/main.c
@@ -3,12 +3,16 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <signal.h>
+#include <pthread.h>
 #include "SerialManager.h"
 
+#define size 128
+
 //Buena práctica utilizar un flag de este tipo de datos dentro del Handler
-volatile sig_atomic_t flagSIGINT;
+volatile sig_atomic_t flagSIGINT = 0;
 
 void SIGINT_Handler(int sig)
 {
@@ -24,17 +28,50 @@ void SIGINT_Handler(int sig)
 void configuraSIGINT ( void );
 void configuraSIGNALS( void );
 
+char bufferRx[size];
+int cantBytes;
+void* serial_thread ( void* )
+{
+    while(flagSIGINT==0)
+    {
+        cantBytes = serial_receive(bufferRx,size);
+        if(cantBytes>0 && cantBytes<255)
+        {
+            printf("Recibí %d bytes: %s\r\n",cantBytes, bufferRx);
+        }
+        sleep(0.1);
+    }
+    return NULL;
+}
+
+void* TCP_thread ( void* )
+{
+    int cont = 0;
+    char bufferTx[size];
+    while(flagSIGINT==0)
+    {
+        int recibiPaqueteTCP = 0;
+        if(recibiPaqueteTCP==1)
+        {
+            // ----- Leer paquete TCP ----- //
+            //guardar en bufferRx
+
+            // ---- Reenviar a Emulador ---//
+            //snprintf(bufferTx,sizeof(bufferTx),">OUT:%d,%d\r\n",X,Y);
+            printf("Recibí y Envío: %s\r\n",bufferRx);
+            serial_send(bufferTx,strlen(bufferTx));
+        }
+        //printf("Hilo TCP %d\r\n",cont++);
+        sleep(0.1);
+    }
+    return NULL;
+}
+
 int main(void)
 {
     printf("Inicio Serial Service\r\n");
 
     configuraSIGNALS();
-
-    int X=0;
-	int Y=0;
-	uint8_t size = 128;
-	char bufferTx[size];
-	char bufferRx[size];
 
 	if( serial_open(1,115200)!=0 )
 	{
@@ -42,25 +79,13 @@ int main(void)
         return -1;
 	}
 
+	pthread_t t1, t2;
+	pthread_create(&t1,  NULL, serial_thread, NULL);
+	pthread_create(&t2,  NULL, TCP_thread,    NULL);
 
-	uint8_t cantBytes;
-    flagSIGINT = 0;
-	while(flagSIGINT==0)
-	{
-        if(Y==0)        Y = 1;
-        else if(Y==1)   Y = 0;
+    pthread_join(t1,NULL);
+    pthread_join(t2,NULL);
 
-        snprintf(bufferTx,sizeof(bufferTx),">OUT:%d,%d\r\n",X,Y);
-        printf("Envio: %s\r\n",bufferTx);
-        serial_send(bufferTx,strlen(bufferTx));
-
-        cantBytes = serial_receive(bufferRx,size);
-        if(cantBytes>0 && cantBytes<255)
-        {
-            printf("Recibí %d bytes: %s\r\n",cantBytes, bufferRx);
-        }
-        sleep(1);
-	}
 	printf("Me voy, cierro el puerto serie\r\n");
     serial_close();
 	exit(EXIT_SUCCESS);
