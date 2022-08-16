099eaeb1f9c65870e5fb19ecfb958bcb5c9ca2ad
Joan Emmanuel Jordán <ejjordan91@gmail.com>
2022-08-16 14:34:12 -0300

    agrego comunicacion TCP en otro hilo

--- Tp_Final_Jordan/SerialService/main.c
+++ Tp_Final_Jordan/SerialService/main.c
@@ -2,11 +2,17 @@
 
 #include <stdio.h>
 #include <stdint.h>
+#include <sys/types.h>
+#include <sys/socket.h>
 #include <stdlib.h>
+#include <errno.h>
 #include <string.h>
 #include <unistd.h>
 #include <signal.h>
 #include <pthread.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
 #include "SerialManager.h"
 
 #define size 128
@@ -30,16 +36,23 @@ void configuraSIGINT    ( void );
 void configuraSIGNALS   ( void );
 void bloquearSIGNALS    ( void );
 void desbloquearSIGNALS ( void );
+void configuraTCP       ( void );
 
 char bufferRx[size];
 int cantBytes;
 void* serial_thread ( void* )
 {
+    if( serial_open(1,115200)!=0 )
+	{
+        perror("Error abriendo puerto serie\r\n");
+        exit(1);
+	}
+
     while(flagMain==0)
     {
         cantBytes = serial_receive(bufferRx,size);
         if(cantBytes>0 && cantBytes<255)
-        {flagSIGINT
+        {
             printf("Recibí %d bytes: %s\r\n",cantBytes, bufferRx);
         }
         sleep(0.1);
@@ -49,25 +62,99 @@ void* serial_thread ( void* )
 
 void* TCP_thread ( void* )
 {
-    /*int cont = 0;
-    char bufferTx[size];
-    while(flagMain==0)
+    //---------- Configuración TCP --------//
+    // Según el tipo de socket se tienen distintos tipos de estructuras para inicializar
+    // Para el caso de Internet Socket, sockaddr (ipv4) contiene IP+PUERTO
+	socklen_t addr_len;
+	struct sockaddr_in clientaddr;
+	struct sockaddr_in serveraddr;
+	char bufferTx[128];
+	int fd_com;         //file descriptor para comunicarse con el Cliente
+	int n;              //bytes recibidos del cliente
+
+	// Creamos socket de Internet (AF_INET), tipo Stream Socket (TCP)
+    // fd_listen: file descriptor que escuchará conexiones Entrantes
+	int fd_listen = socket(PF_INET,SOCK_STREAM, 0);
+
+	// El socket fue creado pero aún no tiene asignada ninguna dirección
+	// Cargamos dirección del server (IP+PUERTO) en estructura
+    bzero( (char*) &serveraddr, sizeof(serveraddr));
+    serveraddr.sin_family = AF_INET;        //Internet Socket
+    serveraddr.sin_port = htons(10000);      //Puerto donde escuchará el Server
+    //htons: Host (mi procesador) to Network SHORT
+    //serveraddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    //pton: Presentation (formato humano) to Network
+    serveraddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    if(serveraddr.sin_addr.s_addr==INADDR_NONE)
     {
-        int recibiPaqueteTCP = 0;
-        if(recibiPaqueteTCP==1)
+        fprintf(stderr,"ERROR invalid server IP\r\n");
+        exit(1);
+    }
+
+	// Abrimos puerto con bind(),
+	// Establece la DIRECCIÓN LOCAL del socket (IP+PUERTO)
+	// para saber a qué socket establecer dirección, se le envía su fd
+	if (bind(fd_listen, (struct sockaddr*)&serveraddr, sizeof(serveraddr)) == -1)
+	{
+		close(fd_listen);
+		perror("listener: bind");
+		exit(1);
+	}
+
+	// Seteamos socket en modo Listening
+	// listen() permite al socket escuchar conexiones entrantes
+	if (listen (fd_listen, 10) == -1) // backlog=10
+  	{
+    	perror("error en listen");
+    	exit(1);
+  	}
+
+  	//---------- Bucle TCP --------//
+  	int socketConectado = 0;
+	while(flagMain==0)
+	{
+		addr_len = sizeof(struct sockaddr_in);
+		printf  ("Esperando aceptar nuevo cliente...\n");
+
+		//Intento aceptar conexión de nuevo Cliente
+		socketConectado = 0;
+    	if ( (fd_com = accept(fd_listen, (struct sockaddr *)&clientaddr, &addr_len)) == -1)
+      	{
+		      perror("error en accept");
+		      exit(1);
+	    }
+	    else
+	    {
+            socketConectado = 1;
+            printf  ("server:  conexion desde:  %s\n", inet_ntoa(clientaddr.sin_addr));
+	    }
+
+        while(socketConectado==1)
         {
-            // ----- Leer paquete TCP ----- //
-            //guardar en bufferRx
+            // --------- Leemos Mensaje del Cliente -------- //
+            if( (n = read(fd_com,bufferTx,128)) == -1 )
+            {
+                perror("Error leyendo mensaje en socket");
+                exit(1);
+            }
+            bufferTx[n]=0;
+            printf("Recibi %d bytes.:%s\n",n,bufferTx);
+            if(n==0) //Se desconectó el ciente (ojo, mejor SIGNAL)
+            {
+                socketConectado = 0;
+                printf  ("Se desconectó el cliente :( \n");
+            }
+            else
+            {
+                serial_send(bufferTx,strlen(bufferTx));
+            }
 
-            // ---- Reenviar a Emulador ---//
-            //snprintf(bufferTx,sizeof(bufferTx),">OUT:%d,%d\r\n",X,Y);
-            printf("Recibí y Envío: %s\r\n",bufferRx);
-            serial_send(bufferTx,strlen(bufferTx));
         }
-        //printf("Hilo TCP %d\r\n",cont++);
-
-    }*/
-    sleep(0.1);
+        // Cerramos lector
+    	close(fd_listen);
+		// Cerramos conexion con cliente
+    	close(fd_com);
+	}
     return NULL;
 }
 
@@ -76,13 +163,7 @@ int main(void)
     printf("Inicio Serial Service\r\n");
 
     configuraSIGNALS();
-    bloquearSIGNALS();  //Bloqueo señales antes de crear hilos
-
-	if( serial_open(1,115200)!=0 )
-	{
-        perror("Error abriendo puerto serie\r\n");
-        return -1;
-	}
+    bloquearSIGNALS();      //Bloqueo señales antes de crear hilos
 
 	pthread_t t1, t2;
 	pthread_create(&t1,  NULL, serial_thread, NULL);
@@ -136,10 +217,10 @@ void bloquearSIGNALS()
     //int s;
     sigemptyset(&set);
     sigaddset(&set,SIGINT);
-    if ( pthread_sigmask(SIG_BLOCK, &set, NULL) == -1 );
+    if ( pthread_sigmask(SIG_BLOCK, &set, NULL) == -1 )
     {
-        perror("Error creando máscara de bloqueo de hilos: ");
-        return 1;
+        printf("Error creando mascara de bloqueo de hilos: ");
+        exit(1);
     }
 }
 
@@ -149,9 +230,14 @@ void desbloquearSIGNALS()
     //int s;
     sigemptyset(&set);
     sigaddset(&set,SIGINT);
-    if ( pthread_sigmask(SIG_UNBLOCK, &set, NULL) == -1 );
+    if ( pthread_sigmask(SIG_UNBLOCK, &set, NULL) == -1 )
     {
         perror("Error creando máscara de bloqueo de hilos: ");
-        return 1;
+        exit(1);
     }
+}
+
+void configuraTCP (void)
+{
+    //
 }
